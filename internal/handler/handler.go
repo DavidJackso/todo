9e7ce4de8a97681287070dc6e9852a11aca91079
David <gutnov3643@gmail.com>
2025-07-03 11:56:46 +0300

    init endpoints

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,36 @@
 package handler
 
+import "github.com/gin-gonic/gin"
+
 type Handler struct {
 }
 
 func NewHanlder() Handler {
 	return Handler{}
+}
+
+func InitRouting() *gin.Engine {
+	router := gin.Default()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("sig-in")
+		auth.POST("sign-up")
+	}
+
+	user := router.Group("/profile")
+	{
+		user.GET("/")
+		user.PATCH("/")
+		user.DELETE("/")
+	}
+
+	tasks := router.Group("/task")
+	{
+		tasks.POST("/")
+		tasks.GET("/:id")
+		tasks.GET("/")
+		tasks.DELETE("/:id")
+	}
+	return router
 }
